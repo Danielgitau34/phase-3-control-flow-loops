699324d617556c4bd10c7cae4921dab3ed193d74
Daniel Gitau <gitaud959@gmail.com>
2023-03-07 13:33:40 +0300

    commit control flo loops

--- looping.rb
+++ looping.rb
@@ -1,24 +1,30 @@
+
 def happy_new_year
   # your code here
+  i = 11
+  while i > 1
+    puts i -= 1
+  end
+    puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    num
-  end
-end
+@@ -17,8 +24,18 @@ def fizzbuzz(num)
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  rev_str = ""
+  str.length.times do |i|
+    #puts rev_str = str[i] + rev_str
+    rev_str = str[i] + rev_str
+  end
+  rev_str
+
 end
